cd8d6a837e19995bdeba69c94982a9739ee87204
__Mazzz__ <dylanmaz@protonmail.com>
2023-01-31 17:36:46 +0000

    Added wifi and firebase setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,39 @@
 #include "Wire.h"       // For I2C/TWI devices to communicate with the Arduino i.e. the temp and humid. sensor and sunlight sensor
 #include "DHT.h"        // For temperature and humidity sensor
 #include "Si115x.h"     // For sunlight sensor
+#include "Firebase_Arduino_WiFiNINA.h"  // For Firebase
 
 //Definitions for constant values
 #define DHTTYPE DHT20   // DHT 20 is the current iteration of the temp & hum. sensor
 DHT dht(DHTTYPE);       // DHT object for calling temp and humidity library methods
 Si115X si1151;          // Sunlight sensor object for calling library methods
+#define FIREBASE_HOST "your-firebase-project-id.firebaseio.com"
+#define FIREBASE_AUTH "Your_Firebase_Database_Secret"
+#define WIFI_SSID "Your WiFi SSID"
+#define WIFI_PASSWORD "Your WiFi Password"
+FirebaseData firebaseData; // Firebase object for calling library methods
 
 void setup() {
   Serial.begin(9600);    // Serial communication speed
   Wire.begin();
   dht.begin();  
+
+  // WiFi setup
+  Serial.println("Connecting to WiFi...");
+  int status = WL_IDLE_STATUS;
+  while(status != WL_CONNECTED) {
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Firebase setup
+  Serial.println("\nConnecting to Firebase...");
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
+  Firebase.reconnectWiFi(true);
   Serial.println("\n################################################"); 
 }
 
