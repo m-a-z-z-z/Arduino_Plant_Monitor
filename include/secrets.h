4096af925422b1187a5b974ce34d7b4a2fc9e15a
__Mazzz__ <dylanmaz@protonmail.com>
2023-02-01 16:48:11 +0000

    Added code to submit data to firebase

---
+++ include/secrets.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define FIREBASE_HOST "plant-monitor-64489.firebaseio.com"
+#define FIREBASE_AUTH "1hYltDtWBkDG50sMrH29FfcfmbbCvsFvdojQ5mRb"
+#define WIFI_SSID "VM9F3C427"
+#define WIFI_PASSWORD "fVajm7xacXav"
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,16 @@
 #include "Wire.h"       // For I2C/TWI devices to communicate with the Arduino i.e. the temp and humid. sensor and sunlight sensor
 #include "DHT.h"        // For temperature and humidity sensor
 #include "Si115x.h"     // For sunlight sensor
-#include "Firebase_Arduino_WiFiNINA.h"  // For Firebase
+#include "Firebase_Arduino_WiFiNINA.h"  // For Firebase, also includes WiFiNINA library
+#include "secrets.h"    // Includes WiFi and Firebase credentials
 
 //Definitions for constant values
 #define DHTTYPE DHT20   // DHT 20 is the current iteration of the temp & hum. sensor
 DHT dht(DHTTYPE);       // DHT object for calling temp and humidity library methods
 Si115X si1151;          // Sunlight sensor object for calling library methods
-#define FIREBASE_HOST "your-firebase-project-id.firebaseio.com"
-#define FIREBASE_AUTH "Your_Firebase_Database_Secret"
-#define WIFI_SSID "Your WiFi SSID"
-#define WIFI_PASSWORD "Your WiFi Password"
-FirebaseData firebaseData; // Firebase object for calling library methods
+FirebaseData firebaseData;    // Firebase object for calling library methods
+String path = "/Plant_Name";  // Name of "table" containing data
+String jsonStr;               // String for storing JSON data
 
 void setup() {
   Serial.begin(9600);    // Serial communication speed
@@ -67,6 +66,42 @@ void loop() {
   Serial.print(si1151.ReadHalfWord_VISIBLE());
   Serial.print("\t\tUV: ");
   Serial.print(si1151.ReadHalfWord_UV());
+
+  // Firebase code
+  // Send data to Firebase with specific path
+  if (Firebase.setInt(firebaseData, path + "/Soil_Moisture", soilMoistureValue)) {
+    Serial.println(firebaseData.dataPath() + " = " + soilMoistureValue);
+  } 
+   if (Firebase.setFloat(firebaseData, path + "/Temp_Humid/Humidity", temp_hum_val[0])) {
+    Serial.println(firebaseData.dataPath() + " = " + temp_hum_val[0]);
+  } 
+  if (Firebase.setFloat(firebaseData, path + "/Temp_Humid/Temperature", temp_hum_val[1])) {
+    Serial.println(firebaseData.dataPath() + " = " + temp_hum_val[1]);
+  }
+  if (Firebase.setInt(firebaseData, path + "/Sunlight/IR", si1151.ReadHalfWord())) {
+    Serial.println(firebaseData.dataPath() + " = " + si1151.ReadHalfWord());
+  }
+  if (Firebase.setInt(firebaseData, path + "/Sunlight/Visible", si1151.ReadHalfWord_VISIBLE())) {
+    Serial.println(firebaseData.dataPath() + " = " + si1151.ReadHalfWord_VISIBLE());
+  }
+  if (Firebase.setFloat(firebaseData, path + "/Sunlight/UV", si1151.ReadHalfWord_UV())) {
+    Serial.println(firebaseData.dataPath() + " = " + si1151.ReadHalfWord_UV());
+  }
+
+  // Push data to Firebase using pushJSON
+  jsonStr = "{\"Soil_Moisture\":" + String(soilMoistureValue) 
+              + ",\"Temp_Humid\":{\"Humidity\":" + String(temp_hum_val[0]) 
+              + ",\"Temperature\":" + String(temp_hum_val[1]) 
+              + "},\"Sunlight\":{\"IR\":" + String(si1151.ReadHalfWord()) 
+              + ",\"Visible\":" + String(si1151.ReadHalfWord_VISIBLE()) 
+              + ",\"UV\":" + String(si1151.ReadHalfWord_UV()) + "}}"
+  ;
+
+  if (Firebase.pushJSON(firebaseData, path + "/2-pushJSON", jsonStr)) {
+    Serial.println(firebaseData.dataPath() + " = " + firebaseData.pushName());
+  } else {
+    Serial.println("Error: " + firebaseData.errorReason());
+  }
 
   Serial.println("\n################################################"); // hashes to separate each loop for readability
   delay(10000); //  10 second delay between loops
