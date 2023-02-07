d8ae5a2321ed4706edbb126393c001ff8f9f9d4b
__Mazzz__ <dylanmaz@protonmail.com>
2023-02-07 16:09:59 +0000

    removed secrets.h added method for wifi & firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,51 @@
 //Libraries
 #include <Arduino.h>
-#include "Wire.h"       // For I2C/TWI devices to communicate with the Arduino i.e. the temp and humid. sensor and sunlight sensor
-#include "DHT.h"        // For temperature and humidity sensor
-#include "Si115x.h"     // For sunlight sensor
+#include "Wire.h"                       // For I2C/TWI devices to communicate with the Arduino i.e. the temp and humid. sensor and sunlight sensor
+#include "DHT.h"                        // For temperature and humidity sensor
+#include "Si115X.h"                     // For sunlight sensor
 #include "Firebase_Arduino_WiFiNINA.h"  // For Firebase, also includes WiFiNINA library
-#include "secrets.h"    // Includes WiFi and Firebase credentials
 
 //Definitions for constant values
-#define DHTTYPE DHT20   // DHT 20 is the current iteration of the temp & hum. sensor
-DHT dht(DHTTYPE);       // DHT object for calling temp and humidity library methods
-Si115X si1151;          // Sunlight sensor object for calling library methods
+#define DHTTYPE DHT20         // DHT 20 is the current iteration of the temp & hum. sensor
+#define FIREBASE_HOST "plant-monitor-64489-default-rtdb.europe-west1.firebasedatabase.app"
+#define FIREBASE_AUTH "1hYltDtWBkDG50sMrH29FfcfmbbCvsFvdojQ5mRb"
+#define WIFI_SSID "VM9F3C427"
+#define WIFI_PASSWORD "fVajm7xacXav"
+DHT dht(DHTTYPE);             // DHT object for calling temp and humidity library methods
+Si115X si1151;                // Sunlight sensor object for calling library methods
 FirebaseData firebaseData;    // Firebase object for calling library methods
 String path = "/Plant_Name";  // Name of "table" containing data
 String jsonStr;               // String for storing JSON data
 
-void setup() {
-  Serial.begin(9600);    // Serial communication speed
-  Wire.begin();
-  dht.begin();  
-
-  // WiFi setup
+void wifi_connect() {
+    // WiFi setup
   Serial.println("Connecting to WiFi...");
   int status = WL_IDLE_STATUS;
   while(status != WL_CONNECTED) {
     status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print(".");
-    delay(1000);
+    delay(300);
   }
   Serial.println("Connected to WiFi");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+}
 
+void firebase_connect() {
   // Firebase setup
   Serial.println("\nConnecting to Firebase...");
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
   Firebase.reconnectWiFi(true);
+}
+
+void setup() {
+  Serial.begin(9600);     // Serial communication speed
+  Wire.begin();
+  dht.begin();
+
+  wifi_connect();
+  firebase_connect();
+
   Serial.println("\n################################################"); 
 }
 
@@ -66,6 +77,7 @@ void loop() {
   Serial.print(si1151.ReadHalfWord_VISIBLE());
   Serial.print("\t\tUV: ");
   Serial.print(si1151.ReadHalfWord_UV());
+  Serial.println();
 
   // Firebase code
   // Send data to Firebase with specific path
@@ -106,3 +118,4 @@ void loop() {
   Serial.println("\n################################################"); // hashes to separate each loop for readability
   delay(10000); //  10 second delay between loops
 }
+
