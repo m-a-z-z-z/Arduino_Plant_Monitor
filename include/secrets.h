e371e4e76be2a2354cfad1c1f89ce6574e0605bb
__Mazzz__ <dylanmaz@protonmail.com>
2023-05-02 02:00:22 +0100

    Create secrets.h

---
+++ include/secrets.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define FIREBASE_HOST "plant-monitor-64489-default-rtdb.europe-west1.firebasedatabase.app"
+#define FIREBASE_AUTH "1hYltDtWBkDG50sMrH29FfcfmbbCvsFvdojQ5mRb"
+#define WIFI_SSID "YOUR SSID HERE"
+#define WIFI_PASSWORD "WIFI PASSWORD HERE"
+
+String userName = "YOUR APP USERNAME HERE";    // Case sensitive
+String plantID = "PLANT ID HERE"; // Case sensitive
